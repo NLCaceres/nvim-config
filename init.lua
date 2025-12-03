e68204ac53b43e26c2a31d2cc416fd7f8c9b3ea6
NLCaceres <NLCaceres21@Gmail.com>
2025-12-03 01:48:17 -0800

    Re-organize `LspAttach` autocommand keymaps + its ref highlight autocommand group
    - Also adds re-written helper comments to each keymap, now organized from simplest
    (Press 'K' to show docs) to `telescope`-related (Goto Def) and finally LSP-specific (Rename var)
      - Toggle inlay hints comes last since isn't always supported by LSPs
    - The reference highlight autocommand finishes off the `LspAttach` config func
    since it's fairly complicated, using 5 different events, and isn't actually a keymap

--- init.lua
+++ init.lua
@@ -422,42 +422,35 @@ require('lazy').setup({
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
-          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+          -- Open a documentation popup for the highlighted word - Move away to disappear
+          -- See `:help K` for why this keymap; TLDR: It runs [K]eywordprg command
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
 
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
-          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-
-          -- Find references for the word under your cursor.
-          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header.
-          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
+          -- Fuzzy find ALL current document symbols, i.e. vars, funcs, types, etc
           map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
+          -- Fuzzy find ALL of your workspace symbols, i.e. recursively check ALL files
           map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
+          -- Jump to where the highlighted word was first defined - Jump back via `<C-t>`
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+          -- GO TO DECLARATION, NOT DEFINITION, i.e. a C-header file
+          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+          -- Jump to highlighted word's type definition, NOT where the word was defined
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+
+          -- Jump to implementation of highlighted word, i.e. not the type interface
+          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          -- Find references for highlighted word
+          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+          -- Rename highlighted variable, even across files
+          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+          -- Run a code action on highlighted error or suggestion
+          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
           -- This func fixes a diff between Neovim Nightly v0.11 and stable v0.10
           ---@param client vim.lsp.Client
@@ -472,12 +465,16 @@ require('lazy').setup({
             end
           end
 
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
+          -- Enables inlay hints directly inline w/ code IF the LSP supports it (uncommon)
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            map('<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+          end
+
+          -- These autocommands highlight and then un-highlight references to the word
+          -- under your cursor if you pause for a bit. See `:help CursorHold` for info
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -491,7 +488,6 @@ require('lazy').setup({
               group = highlight_augroup,
               callback = vim.lsp.buf.clear_references,
             })
-
             vim.api.nvim_create_autocmd('LspDetach', {
               group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
               callback = function(event2)
@@ -499,13 +495,6 @@ require('lazy').setup({
                 vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
               end,
             })
-          end
-
-          -- Enables inlay hints directly inline w/ code IF the LSP supports it (uncommon)
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
           end
         end,
       })
