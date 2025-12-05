13548e21fafed01ee6b4304e73ea244858a944dd
NLCaceres <NLCaceres21@Gmail.com>
2025-12-05 12:54:49 -0800

    Add `tmux-navigator` to `custom/plugins/init.lua` for easier window switching
    - When using `tmux`, the original keymaps set up to more easily focus different
    windows interfere with the `tmux` keymaps to do the same. With this plugin, the
    two can seamlessly coexist.

--- init.lua
+++ init.lua
@@ -192,21 +192,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- BUT it won't work in all terminals/tmux/etc, so may need to try another map
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- NOTE: Autocommands - See `:help lua-guide-autocommands`
 
 -- Highlight on yank (copying) text - See `:help vim.highlight.on_yank()`
