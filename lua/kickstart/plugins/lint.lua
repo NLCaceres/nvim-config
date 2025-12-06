97951a427aa05f1bffb94e40fea4c545ea9cb64a
NLCaceres <NLCaceres21@Gmail.com>
2025-12-06 15:12:01 -0800

    Re-write `nvim-lint` helper comments to be more concise

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,32 +6,22 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     local lint = require 'lint'
-    lint.linters_by_ft = {
+    lint.linters_by_ft = { -- Actually has defaults BUT better to do the following
       markdown = { 'markdownlint' },
       javascript = { 'eslint_d' },
       typescript = { 'eslint_d' },
-    }
-
-    -- It's possible to let other plugins add linters via below:
+    } -- AND NOT the following which enables 10 linters from clojure to text files
     -- lint.linters_by_ft = lint.linters_by_ft or {}
     -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-    -- BUT it enables the following linters automatically: clojure
-    -- dockerfile, inko, janet, json, markdown, rst, ruby, terraform, and text
-    -- To disable the defaults (and stop their errors), set their filetype to nil
-    -- lint.linters_by_ft['dockerfile'] = nil
-    -- lint.linters_by_ft['json'] = nil
-    -- lint.linters_by_ft['markdown'] = nil
-    -- lint.linters_by_ft['ruby'] = nil
+    -- Requiring you to disable any you don't want (See nvim-lint/blob/master/lua/lint)
+    -- lint.linters_by_ft['clojure'] = nil
 
-    -- Create autocommand which carries out the actual linting
-    -- on the specified events.
+    -- Actually enables the linting based on these events
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
-        -- Only run the linter in buffers that you can modify in order to
-        -- avoid superfluous noise, notably within the handy LSP pop-ups that
-        -- describe the hovered symbol using Markdown.
+        -- Only lint IF the file is editable, i.e. ignore documentation files or tooltips
         if vim.bo.modifiable then
           lint.try_lint()
         end
