aa15cf171003bc6eed0e82f61ed04852eb24bb83
NLCaceres <NLCaceres21@Gmail.com>
2025-12-14 13:18:04 -0800

    Add `render-markdown` plugin for better formatting of `markdown` files
    - Enforces a strict style when writing Markdown files AND renders Markdown
    files in a way that reflects its appearance on Github

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,4 +17,27 @@ return {
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.nvim',
+    },
+    config = function()
+      require('render-markdown').setup {
+        heading = {
+          icons = { '⓵  ', '⓶  ', '⓷  ', '⓸  ', '⓹  ', '⓺  ' },
+        },
+        bullet = {
+          ordered_icons = function(ctx)
+            local value = vim.trim(ctx.value) -- Value == num + '.' i.e. '1.' or '2.' etc
+            local value_index = tonumber(value:sub(1, #value - 1)) --  Grabs number alone
+            -- Return MY number, not the auto-index aka index (which always starts from 1)
+            return string.format('%d.', value_index)
+          end,
+        },
+      }
+    end,
+  },
 }
