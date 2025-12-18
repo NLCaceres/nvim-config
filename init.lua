6490c3ba054fa6f85771acbf6c5957202c3f92d5
NLCaceres <NLCaceres21@Gmail.com>
2025-12-18 12:48:13 -0800

    Enable `grep` current word in Visual mode + add mode to its `which-key` spec
    - Since Telescope's builtin `grep` func can be used directly in Normal mode
    AND by highlighting in Visual mode now, adding to its `which-key` spec
    helps appropriately connect when it's available for use

--- init.lua
+++ init.lua
@@ -320,7 +320,7 @@ require('lazy').setup({
       },
 
       spec = { -- Documents the keymaps
-        { '<leader>s', group = '[S]earch' },
+        { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         { 'gr', group = 'LSP Actions', mode = { 'n' } },
@@ -389,7 +389,7 @@ require('lazy').setup({
       end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sgi', function()
         builtin.git_status { git_icons = { added = '󱇬', changed = '!=' } }
       end, { desc = '[S]earch by [GI]t' })
